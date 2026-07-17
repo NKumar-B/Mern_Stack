2156d7584fa8d735d715b3219f624e76663d152c
NithinKumar-7893 <nithinkumarbadduluri@gmail.com>
2026-07-17 11:04:32 +0530

    Javascript Day3

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+const register = () => {
+    console.log(5*5);
+} 
+register(   );
+
+// document.getElementsByTagName("H1").innerHTML= "Registration Form";
+
+document.getElementById("Title").innerHTML="Registration Form";
